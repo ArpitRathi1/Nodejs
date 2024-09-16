7c916bfd35055ab7882d65f0b5e1d80b73af15b8
ArpitRathi1 <rathi844@gmail.com>
2024-09-16 11:26:16 +0530

    add hello.js

---
+++ 01-Hello-world/hello.js
@@ -0,0 +1,10 @@
+// const math = require("./math")
+const {add, sub} = require("./math")
+
+console.log("Hello, I am Js")
+
+// console.log(math.add(1, 2));       // output : 3
+// console.log(math.sub(4, 2));       // output : 2
+
+console.log(add(3,4));        // output : 7
+console.log(sub(5,4));        // output : 1
